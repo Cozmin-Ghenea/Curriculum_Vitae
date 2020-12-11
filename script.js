f6d66e8f960eba7e2c4c812cabeefc3b964280f3
Cozmin-Ghenea <43807754+Cozmin-Ghenea@users.noreply.github.com>
2020-12-11 19:42:53 +0200

    C.V.
    Curriculum Vitae

---
+++ script.js
@@ -0,0 +1,42 @@
+const navbar = document.querySelector(".navbar");
+
+const navbarOffSetTop = navbar.offsetTop;
+const sections = document.querySelectorAll('section');
+const navbarLinks = document.querySelectorAll('.navbar-link');
+const progress = document.querySelector('.progress-bars-wrapper');
+const prorgessBarPercents = [97,89,75,75,50,35]
+
+window.addEventListener('scroll', () =>{
+    mainFn();
+    
+})
+
+
+const mainFn = () =>{
+    if(window.pageYOffset >=navbarOffSetTop){
+        navbar.classList.add('sticky')
+    }else{
+        navbar.classList.remove('sticky')
+    }
+
+
+sections.forEach((section,i) => {
+    if(window.pageYOffset >=section.offsetTop - 10){
+        navbarLinks.forEach(navbarlink => {
+            navbarlink.classList.remove('change')
+        })
+        navbarLinks[i].classList.add('change');
+    }
+})
+    if(window.pageYOffset + window.innerHeight >= progress.offsetTop){
+        document.querySelectorAll('.progress-percent').forEach((el, i) =>{
+            el.style.width=`${prorgessBarPercents[i]}%`
+            el.previousElementSibling.firstElementChild.textContent=prorgessBarPercents[i]
+        })
+    }
+}
+mainFn();
+
+window.addEventListener('resize', () =>{
+    window.location.reload();
+})
